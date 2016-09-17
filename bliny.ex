30882b52455e1e108413ba50ecd988672beb1243
André Pinheiro <andrepdpinheiro@gmail.com>
2016-09-17 19:57:06 +0100

    First version written in elixir

---
+++ bliny.ex
@@ -0,0 +1,33 @@
+defmodule Bliny do
+  def bliny(order) do
+    m = List.first(order)
+
+    {s, _} = Enum.reduce(order, {Enum.into(1..m, []), m}, fn(x, acc) ->
+      {list, m} = acc
+      {list, m} = eval_diff(list, m, x)
+      {list, m} = eval_eq(list, m, x)
+
+      {list, m}
+    end)
+
+    length(s) == 0
+  end
+
+
+  def eval_diff(list, m, x) do
+    if List.last(list) != x do
+      {list ++ ((m + 1)..x |> Enum.to_list), m}
+    elsecle
+      {list, m}
+    end
+  end
+
+
+  def eval_eq(list, m, x) do
+    if List.last(list) == x do
+      {List.delete_at(list, length(list) - 1), Enum.max [x, m]}
+    else
+      {list, m}
+    end
+  end
+end
