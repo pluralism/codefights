27eef91214706156c6497837b888f4222192ac97
André Pinheiro <andrepdpinheiro@gmail.com>
2016-09-17 20:02:47 +0100

    Refactored code

--- bliny.ex
+++ bliny.ex
@@ -1,33 +1,28 @@
 defmodule Bliny do
   def bliny(order) do
-    m = List.first(order)
-
-    {s, _} = Enum.reduce(order, {Enum.into(1..m, []), m}, fn(x, acc) ->
+    {s, _} = Enum.reduce(order, {Enum.into(1..List.first(order), []), List.first(order)}, fn(x, acc) ->
       {list, m} = acc
       {list, m} = eval_diff(list, m, x)
-      {list, m} = eval_eq(list, m, x)
-
-      {list, m}
+      eval_eq(list, m, x)
     end)
-
     length(s) == 0
   end
 
 
   def eval_diff(list, m, x) do
-    if List.last(list) != x do
-      {list ++ ((m + 1)..x |> Enum.to_list), m}
-    else
-      {list, m}
+    cond do
+      List.last(list) != x ->
+        {list ++ ((m + 1)..x |> Enum.to_list), m}
+      true -> {list, m}
     end
   end
 
 
   def eval_eq(list, m, x) do
-    if List.last(list) == x do
-      {List.delete_at(list, length(list) - 1), Enum.max [x, m]}
-    else
-      {list, m}
+    cond do
+      List.last(list) == x ->
+        {List.delete_at(list, length(list) - 1), Enum.max [x, m]}
+      true -> {list, m}
     end
   end
 end
