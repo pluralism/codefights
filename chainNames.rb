b9d1ed9fd800d48a9415d69292bab33e0df62868
André Pinheiro <andrepdpinheiro@gmail.com>
2016-09-23 22:31:25 +0100

    Create chainNames.rb

---
+++ chainNames.rb
@@ -0,0 +1,13 @@
+def chainNames n
+  conns = Hash.new
+  n.map { |n1|
+    t = n.select { |n2| n1[-1] == n2[0].downcase }
+    conns[n1] = t.empty? ? nil : t.first
+  }
+  n3, r = (conns.keys - conns.values).first, []
+  (1..n.length).each do |i|
+    r << n3
+    n3 = conns[n3]
+  end
+  r
+end
