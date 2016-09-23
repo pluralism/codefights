a0ff704d9843d66d033dea9df8ee8e0dfdaa529e
André Pinheiro <andrepdpinheiro@gmail.com>
2016-09-23 22:33:26 +0100

    Remove counter variable

--- chainNames.rb
+++ chainNames.rb
@@ -5,7 +5,7 @@ def chainNames n
     conns[n1] = t.empty? ? nil : t.first
   }
   n3, r = (conns.keys - conns.values).first, []
-  (1..n.length).each do |i|
+  (1..n.length).each do
     r << n3
     n3 = conns[n3]
   end
