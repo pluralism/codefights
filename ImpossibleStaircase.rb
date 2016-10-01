6075f3722e71858d7887da3b5bd11bcede4c4f60
André Pinheiro <andrepdpinheiro@gmail.com>
2016-10-01 13:46:28 +0100

    Progress

--- ImpossibleStaircase.rb
+++ ImpossibleStaircase.rb
@@ -1,24 +1,3 @@
-def ImpossibleStaircase(staircase)
-  visited = Array.new(staircase.length) { Array.new(staircase[0].length, false) }
-  height = Array.new(staircase.length) { Array.new(staircase[0].length, 1000) }
-  new_staircase = []
-  staircase.each do |n| new_staircase << n.chars end
-  start_node, queue = [], []
-
-  new_staircase.each_index { |i| j = new_staircase[i].index '#'; start_node = [i, j]; break }
-  visited[start_node[0]][start_node[1]] = true
-  queue << start_node
-
-  while !queue.empty?
-    current_node = queue.pop
-    nodes = next_nodes(current_node, staircase)
-    nodes.each do |node|
-      
-    end
-  end
-end
-
-
 def next_nodes(node, staircase)
   nodes = []
   # Below
@@ -30,6 +9,44 @@ def next_nodes(node, staircase)
   # Right
   nodes << [node[0], node[1] + 1] if node[1] + 1 < staircase[0].length
   nodes
+end
+
+
+def ImpossibleStaircase(staircase)
+  visited = Array.new(staircase.length) { Array.new(staircase[0].length, false) }
+  height = Array.new(staircase.length) { Array.new(staircase[0].length, 1000) }
+  new_staircase = []
+  staircase.each do |n| new_staircase << n.chars end
+  start_node, queue = [], []
+
+  new_staircase.each_index { |i| j = new_staircase[i].index '#'; start_node = [i, j]; break }
+  visited[start_node[0]][start_node[1]] = true
+  queue << start_node
+
+  current_node = queue.pop
+  nodes = next_nodes(current_node, staircase)
+  nodes.each do |node|
+    next if staircase[node[0]][node[1]] == ' '
+    val = staircase[node[0]][node[1]]
+    if val == 'v'
+      height[node[0] + 1][node[1]] = height[node[0] - 1][node[1]] - 1
+    elsif val == '<'
+      height[node[0]][node[1] + 1] = height[node[0]][node[1] - 1] + 1
+    elsif val == '>'
+      height[node[0]][node[1] + 1] = height[node[0]][node[1] - 1] - 1
+    end
+    visited[node[0]][node[1]] = true
+    queue << [node[0], node[1]]
+    p queue
+  end
+
+
+  visited.each do |n|
+    p n
+    puts
+  end
+  #while !queue.empty?
+  #end
 end
 
 
