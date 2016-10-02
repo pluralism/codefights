4f3cdc98466615c7e29a3d67b044745a1a57ae24
André Pinheiro <andrepdpinheiro@gmail.com>
2016-10-02 12:43:41 +0100

    IsItPoem is now solved in elixir

--- codefights/lib/is_it_poem.ex
+++ codefights/lib/is_it_poem.ex
@@ -1,5 +1,16 @@
 defmodule IsItPoem do
   def isItPoem(rhyme, text) do
-    
+    Enum.map(Enum.chunk(text, length rhyme), fn chunk ->
+      rhyme_zip = Enum.zip rhyme, chunk
+      Enum.uniq(rhyme)
+      |> Enum.map(fn num ->
+        Enum.filter(rhyme_zip, fn {num2, _} -> num == num2 end)
+        |> Enum.map(fn {_, text} -> String.slice(text, -3..-1) end)
+        |> Enum.uniq
+        |> length
+      end)
+    end)
+    |> List.flatten
+    |> Enum.all?(&(&1 == 1))
   end
 end
