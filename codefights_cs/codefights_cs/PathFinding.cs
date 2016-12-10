6c971707785f933a3dd7f645f28418e1963b96a4
André Pinheiro <andrepdpinheiro@gmail.com>
2016-12-10 14:04:43 +0000

    Adding C# code

---
+++ codefights_cs/codefights_cs/PathFinding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace codefights_cs
+{
+    class PathFinding
+    {
+        public int pathfinding(int start, int[][] edges, int[] markedNodes)
+        {
+
+
+            return 0;
+        }
+    }
+}
