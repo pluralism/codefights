a39f9252aba9c6a5dc561b11a345aafe9f79b89e
André Pinheiro <andrepdpinheiro@gmail.com>
2016-12-10 15:54:06 +0000

    Pathfinding challenge in C#

--- codefights_cs/codefights_cs/PathFinding.cs
+++ codefights_cs/codefights_cs/PathFinding.cs
@@ -9,11 +9,37 @@ namespace codefights_cs
 {
     class PathFinding
     {
+        /*
+         * Given a graph as a list of its edges, your task is to find the shortest path that starts at a start 
+         * and visits each of the markedNodes exactly once. 
+         * If it's impossible to do this, return -1 instead.
+         */
         public int pathfinding(int start, int[][] edges, int[] markedNodes)
         {
+            // Create a Dictionary for faster edge lookup
+            Dictionary<int, List<int>> edgesDict = new Dictionary<int, List<int>>();
 
+            List<int> outVal;
+            // Iterate over the list of all edges to create the keys
+            foreach (int[] edge in edges)
+            {
+                /**
+                 * edge[0] represents the source vertex
+                 * edge[1] represents the targe vertex
+                 */
+                if (!edgesDict.TryGetValue(edge[0], out outVal))
+                    edgesDict.Add(edge[0], new List<int>());
+            }
 
-            return 0;
+            /**
+             * Create the connections between the vertexes
+             * 
+             * Each source vertex can be conneced to a list of target vertexes
+             */
+            foreach (int[] edge in edges)
+                edgesDict[edge[0]].Add(edge[1]);
+
+            return -1;
         }
     }
 }
--- codefights_cs/codefights_cs/Program.cs
+++ codefights_cs/codefights_cs/Program.cs
@@ -10,7 +10,57 @@ namespace codefights_cs
     {
         static void Main(string[] args)
         {
+            int[][] edges = new int[][]
+            {
+                new int[] { 2, 5 },
+                new int[] { 3, 4 },
+                new int[] { 4, 7 },
+                new int[] { 5, 6 },
 
+                new int[] { 6, 9 },
+                new int[] { 6, 5 },
+                new int[] { 7, 8 },
+                new int[] { 8, 14 },
+
+                new int[] { 8, 25 },
+                new int[] { 9, 10 },
+                new int[] { 14, 13 },
+                new int[] { 11, 12 },
+
+                new int[] { 12, 13 },
+                new int[] { 13, 27 },
+                new int[] { 14, 13 },
+                new int[] { 16, 17 },
+
+                new int[] { 16, 8 },
+                new int[] { 17, 18 },
+                new int[] { 18, 19 },
+                new int[] { 18, 6 },
+
+                new int[] { 19, 20 },
+                new int[] { 19, 9 },
+                new int[] { 19, 22 },
+                new int[] { 20, 23 },
+
+                new int[] { 21, 22 },
+                new int[] { 21, 27 },
+                new int[] { 22, 19 },
+                new int[] { 22, 21 },
+
+                new int[] { 23, 24 },
+                new int[] { 23, 18 },
+                new int[] { 24, 20 },
+                new int[] { 25, 6 },
+
+                new int[] { 27, 16 }
+            };
+
+            int[] markedNodes = new int[] { 25, 27, 5 };
+
+
+            PathFinding pf = new PathFinding();
+            pf.pathfinding(0, edges, markedNodes);
+            Console.ReadLine();
         }
     }
 }
