b9029e6b82273e3e3b434a292bf4b07d71b676ad
André Pinheiro <andrepdpinheiro@gmail.com>
2016-12-10 16:20:25 +0000

    Coding the BFS code

--- codefights_cs/codefights_cs/PathFinding.cs
+++ codefights_cs/codefights_cs/PathFinding.cs
@@ -41,5 +41,47 @@ namespace codefights_cs
 
             return -1;
         }
+
+
+        public void BFS(int start, Dictionary<int, List<int>> edgesDict)
+        {
+            /**
+             * Each element in the Queue Q is composed by 
+             * a tuple that contains the following elements:
+             * 
+             *  - Node
+             *  - Cost  
+             */
+            Queue<Tuple<int, int>> Q = new Queue<Tuple<int, int>>();
+            Q.Enqueue(Tuple.Create(start, 0));
+
+            // Contains the vertexes that were already discovered
+            HashSet<int> discovered = new HashSet<int>();
+
+            // Holds the cost of the shortest path between the start and all other vertexes
+            Dictionary<int, int> costs = new Dictionary<int, int>();
+
+            // Run while the Queue object is not empty
+            while(Q.Count > 0)
+            {
+                // Removes and returns the first inserted element (node + cost associated)
+                Tuple<int, int> current = Q.Dequeue();
+
+                // The vertex was already discovered, just continue...
+                if (discovered.Contains(current.Item1))
+                    continue;
+
+                // The vertex was not discovered yet, add it to the discovered vertexes
+                discovered.Add(current.Item1);
+
+                /**
+                 * Add the information that represents the cost from reaching the
+                 * current Node from the start node
+                 * 
+                 * The key is the node and the value is the cost
+                 */
+                costs.Add(current.Item1, current.Item2);
+            }
+        }
     }
 }
