e28024e6cd2d18e5a9cd02bff1ca2352091e9e07
André Pinheiro <andrepdpinheiro@gmail.com>
2017-01-24 19:47:32 +0000

    Updating code

---
+++ codefights_cs/codefights_cs/PossibleRing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace codefights_cs
+{
+    public class PossibleRing
+    {
+        public static int possibleRing(string formula)
+        {
+            Regex pattern = new Regex();
+
+            return 6;
+        }
+    }
+}
